e374019f36593ba4101a3c1b40e916f1c8f862fd
whatoson <117279178+whatoson@users.noreply.github.com>
2026-04-16 17:47:12 +0700

    feat: setup pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,36 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Notepad",
+        short_name: "Notepad",
+        description: "Notepad application",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+      workbox: {
+        globPatterns: [
+          "**/*.{js,css,html,ico,png,svg,woff,woff2,json,xml,webmanifest}",
+        ],
+      },
+    }),
+  ],
+});
