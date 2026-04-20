c7544dd98dde3af0d4a793acf89f3871649dbd83
whatoson <117279178+whatoson@users.noreply.github.com>
2026-04-20 13:13:50 +0700

    feat: show current note's title in header

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,10 +1,15 @@
 import { SidebarTrigger } from "@/components/ui/sidebar";
+import { useNoteList } from "@/features/noteList/useNoteList";
+import { useParams } from "react-router";
 
 export function Header() {
+  const { noteId } = useParams();
+  const note = useNoteList((s) => s.notes.find((n) => n.id === noteId));
+
   return (
     <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
-      <SidebarTrigger className="-ml-1" />
-      <p>Header</p>
+      <SidebarTrigger />
+      <p>{note ? note.title : "Notepad"}</p>
     </header>
   );
 }
